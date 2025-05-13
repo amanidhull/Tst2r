ed477c48595cd0e00174544190e61c9f0e5811bd
Amani <197231008+amanidhull@users.noreply.github.com>
2025-05-14 00:01:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,6 +2,8 @@ import os
 import re
 from os import environ,getenv
 from Script import script
+import asyncio
+from utils import get_search_results
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
